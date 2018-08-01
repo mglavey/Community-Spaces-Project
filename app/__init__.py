4f080e9c5263d017ce248492e55086d4c0706333
Meg Glavey <glaveym@gmail.com>
2018-08-01 11:26:15 -0500

    wed

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_assets import Bundle, Environment
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -12,5 +13,6 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+
 
 from app import routes, models
